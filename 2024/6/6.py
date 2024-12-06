f5afbc1d6ab771b7992fbb678a3bcd32cc52e825
Victor Dahlsberg <victordahlsberg@hotmail.com>
2024-12-06 16:47:24 +0100

    c

--- 2024/6/6.py
+++ 2024/6/6.py
@@ -1,81 +1,58 @@
+from collections import defaultdict
 input = open("input").read().strip()
 
+
+def get_visited(pos, grid):
+    visited = set()
+    visited_dirrs = defaultdict(set)
+    dirr = 0
+
+    while True:
+        visited.add(pos)
+        if dirr in visited_dirrs[pos]:
+            return visited, True
+        visited_dirrs[pos].add(dirr)
+        directions = [(pos[0], pos[1] - 1), (pos[0] + 1, pos[1]),
+                      (pos[0], pos[1] + 1), (pos[0] - 1, pos[1])]
+
+        while True:
+            nextPos = directions[dirr]
+            if not (len(grid[0]) > nextPos[0] >= 0 and len(grid) > nextPos[1] >= 0):
+                return visited, False
+            elif grid[nextPos[1]][nextPos[0]] == ".":
+                break
+            dirr = (dirr + 1) % 4
+            nextPos = directions[dirr]
+
+        pos = nextPos
+
+
+def get_walls(visited):
+    walls = set()
+    for x, y in visited[0]:
+        if grid[y][x] == ".":
+            grid[y][x] = "#"
+            if get_visited((42, 83), grid)[1]:
+                walls.add((x, y))
+            grid[y][x] = "."
+    return walls
+
+
+def get_walls_2():
+    walls = set()
+    for y in range(130):
+        for x in range(130):
+            if grid[y][x] == ".":
+                grid[y][x] = "#"
+                if get_visited((42, 83), grid)[1]:
+                    walls.add((x, y))
+                grid[y][x] = "."
+    return walls
+
+
 grid = [[c for c in line] for line in input.split("\n")]
-print(grid)
 
-posX = 42
-posY = 83
-visited = set()
-dirr = 0
-
-while True:
-    u = (posX, posY-1)
-    r = (posX+1, posY)
-    d = (posX, posY+1)
-    l = (posX-1, posY)
-    print(posX, posY)
-    print(dirr)
-    if dirr == 0:
-        if u[1] < 0:
-            break;
-        if grid[u[1]][u[0]] == ".":
-            posX = u[0]
-            posY = u[1]
-        elif grid[u[1]][u[0]] == "#" and grid[r[1]][r[0]] != "#":
-            dirr = (dirr + 1) % 4
-            posX = r[0]
-            posY = r[1]
-        else:
-            dirr = (dirr + 2) % 4
-            posX = d[0]
-            posY = d[1]
-        visited.add((posX, posY))
-
-    elif dirr == 1:
-        if r[0] >= len(grid[0])-1:
-            break;
-        if grid[r[1]][r[0]] == ".":
-            posX = r[0]
-            posY = r[1]
-        elif grid[r[1]][r[0]] == "#" and grid[d[1]][d[0]] != "#":
-            dirr = (dirr + 1) % 4
-            posX = d[0]
-            posY = d[1]
-        else:
-            dirr = dirr + 2 % 4
-            posX = l[0]
-            posY = l[1]
-        visited.add((posX, posY))
-
-    elif dirr == 2:
-        if d[1] >= len(grid)-1:
-            break;
-        if grid[d[1]][d[0]] == ".":
-            posX = d[0]
-            posY = d[1]
-        elif grid[d[1]][d[0]] == "#" and grid[l[1]][l[0]] != "#":
-            dirr = (dirr + 1) % 4
-            posX = l[0]
-            posY = l[1]
-        else:
-            dirr = (dirr + 2) % 4
-            posX = u[0]
-            posY = u[1]
-        visited.add((posX, posY))
-    elif dirr == 3:
-        if l[0] < 0:
-            break;
-        if grid[l[1]][l[0]] == ".":
-            posX = l[0]
-            posY = l[1]
-        elif grid[l[1]][l[0]] == "#" and grid[u[1]][u[0]] != "#":
-            dirr = (dirr + 1) % 4
-            posX = u[0]
-            posY = u[1]
-        else:
-            dirr = (dirr + 2) % 4
-            posX = r[0]
-            posY = r[1]
-        visited.add((posX, posY))
-
-print(len(visited))
+visited = get_visited((42, 83), grid)
+print(len(visited[0]))
+print(len(get_walls(visited)))
+print(len(get_walls_2()))
